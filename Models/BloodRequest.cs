e89262c12431c0b30bbf2a3e74d419e5ac012061
fatimabibi-21 <bibif6267@gmail.com>
2026-01-09 23:45:19 +0500

    Add files via upload

---
+++ Models/BloodRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloodLink.Models
+{
+    public class BloodRequest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string PatientName { get; set; }
+
+        [Required]
+        public string BloodGroup { get; set; }
+
+        [Required]
+        public string HospitalName { get; set; }
+
+        [Required]
+        public string ContactNumber { get; set; }
+
+        // Allow NULLs from the DB; keep a default when creating new objects.
+        public DateTime? RequestDate { get; set; } = DateTime.Now;
+
+        public string Status { get; set; } = "Pending";
+        public string Priority { get; set; } = "Normal"; // Default value
+    }
+}
---
+++ Models/Donor.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloodLink.Models
+{
+    public class Donor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string BloodGroup { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+    }
+}
