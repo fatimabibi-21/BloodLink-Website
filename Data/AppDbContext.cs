07685a80b5d1bb7d84675d175b7bcf64f0c31e62
fatimabibi-21 <bibif6267@gmail.com>
2026-01-09 23:44:52 +0500

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BloodLink.Models;
+
+namespace BloodLink.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // this wwill say "There is a table called Donors, treat it like a list of Donor objects"
+        public DbSet<Donor> Donors { get; set; }
+
+        // The BloodRequests Table 
+        public DbSet<BloodRequest> BloodRequests { get; set; }
+    }
+}
